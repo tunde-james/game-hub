030bc188edf3e70e7187cad6d923d1166c9f16f3
Tunde James <tundejames04@gmail.com>
2023-08-31 19:08:10 +0100

    Display genre icons

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function GameCardContainer({ children }: Props) {
   return (
-    <Box borderRadius={10} overflow="hidden" width="300px">
+    <Box borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
